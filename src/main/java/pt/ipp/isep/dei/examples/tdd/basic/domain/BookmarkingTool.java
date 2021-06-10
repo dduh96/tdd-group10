9dba654080bef7c4d58bc4eb3324e18d052f21d1
Katharina <katharina.buehn@hotmail.com>
2021-06-10 12:15:19 +0200

    deleteBookmark Case1 code

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkingTool.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkingTool.java
@@ -115,7 +115,18 @@ public class BookmarkingTool {
     }
 
     public boolean deleteBookmark(String url){
-        return true;
+        boolean bookmarkDeleted = false;
+        if (url == null || savedBookmarks.isEmpty())
+            return false;
+        Iterator<Bookmark> bookmark = savedBookmarks.iterator();
+        while (bookmark.hasNext() && !bookmarkDeleted) {
+            Bookmark nextBookmark = bookmark.next();
+            if (nextBookmark.getUrl().equals(url)) {
+                savedBookmarks.remove(nextBookmark);
+                bookmarkDeleted = true;
+            }
+        }
+        return bookmarkDeleted;
     }
 
 }
